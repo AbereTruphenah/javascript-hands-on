15832d50ec5983d898486411db6a6eb232baf3c5
AbereTruphenah <145029217+AbereTruphenah@users.noreply.github.com>
2025-02-01 10:49:34 +0300

    prompt() function

---
+++ EXERCISES/prompt-function.js
@@ -0,0 +1,3 @@
+/* prompt() creates a pop-up window that lets the user input text. 
+The window displays a message, an input field, and "OK" and "Cancel" buttons. */
+prompt("Please enter your name:", "June");
