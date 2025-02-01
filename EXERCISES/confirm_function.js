7950a75751e669f3f35128baee34cbdf0ccca15b
AbereTruphenah <145029217+AbereTruphenah@users.noreply.github.com>
2025-02-01 10:49:14 +0300

    confirm() function

---
+++ EXERCISES/confirm_function.js
@@ -0,0 +1,3 @@
+/* presents a dialog with a message and two buttons: "OK" and "Cancel." 
+It returns true if the user clicks "OK" and false if they click "Cancel."*/
+confirm("Are you sure you want to proceed?");
