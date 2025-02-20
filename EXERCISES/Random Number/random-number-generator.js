2096590c14f966ce738b7a21d1e79d42f9725e5d
AbereTruphenah <145029217+AbereTruphenah@users.noreply.github.com>
2025-02-20 15:54:53 +0300

    Random Number Generation

---
+++ EXERCISES/Random Number/random-number-generator.js
@@ -0,0 +1,5 @@
+//let randomNum=Math.random();// returns a random number between 0 and 1.
+//let randomNum=Math.floor(Math.random());//rounds down the generated random number.
+//let randomNum=Math.floor(Math.random()*5);//random number between 0 and 5; 5 excluded.
+let randomNum=Math.floor(Math.random()*5)+1;//increases the generated random number by 1
+console.log(randomNum);
