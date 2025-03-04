387d08cca5563b1e6881c6b710ba74cac8a87b10
AbereTruphenah <145029217+AbereTruphenah@users.noreply.github.com>
2025-03-04 11:30:24 +0300

    updated

--- TEMPERATURE CONVERTOR/script.js
+++ TEMPERATURE CONVERTOR/script.js
@@ -1,28 +1,29 @@
 // Get references to the HTML elements by their IDs
-const textBox = document.getElementById("textBox");// Input field for temperature value
-const toCelcius = document.getElementById("toCelcius");// Radio button for Celcius conversion
-const toFahrenhelt = document.getElementById("toFahrenhelt");// Radio button for Fahrenheit conversion
-const result = document.getElementById("result");// Element to display the result
+const textBox = document.getElementById("textBox"); // Input field for temperature value
+const toCelsius = document.getElementById("toCelsius"); // Radio button for Celsius conversion
+const toFahrenheit = document.getElementById("toFahrenheit"); // Radio button for Fahrenheit conversion
+const result = document.getElementById("result"); // Element to display the result
 
-let temp;// Variable to store the converted temperature
+// Variable to store the converted temperature
+let temp;
 //Function to convert temperature based on selected unit
 function convert() {
     //check if Fahrenheit button is selected
-    if (toFahrenhelt.checked) {
+    if (toFahrenheit.checked) {
         //convert the input valueto a numbner
         temp = Number(textBox.value);
-        // Covert input to FaHrenHeit
+        // Covert input to Fahrenheit
         temp = temp * 9 / 5 + 32;
         //Display output with no decimal places
         result.textContent = temp.toFixed() + "F";
 
 
     }
-    //Check if Celcius button is selected
-    else if (toCelcius.checked) {
+    //Check if Celsius button is selected
+    else if (toCelsius.checked) {
         //Convert input value to number
         temp = Number(textBox.value);
-        // Convert Fahrenheit to Celcius
+        // Convert Fahrenheit to Celsius
         temp = (temp - 32) * (5 / 9);
         //Display result with no decimal places
         result.textContent = temp.toFixed() + "C";
